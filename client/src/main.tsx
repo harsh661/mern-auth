7f771284af77b1b3ae367ff2bd74380813e15349
harsh661 <plasmaharsh@gmail.com>
2023-09-12 09:57:55 +0530

    feat: initialize browser router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import '../index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "../index.css"
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
